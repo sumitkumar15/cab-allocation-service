f76dd4e8a94072691155e22b21dc0ff2a51fce4c
sumit <kumar1993sumit15@gmail.com>
2018-06-08 17:37:04 +0530

    handler for ending rides

--- routes/cabhandler.js
+++ routes/cabhandler.js
@@ -2,15 +2,32 @@ var express = require('express')
 var router = express.Router()
 var allocator = require('../bookingservice/AllocatorService')
 const {Customer} = require('../bookingservice/Profiles')
+const {Location} = require('../bookingservice/utils')
 const bodyParser = require('body-parser')
 
 router.use(bodyParser.json())
 
 router.post('/hire', (req, res) => {
-  const params = req.params
   const body = req.body
   var cust = new Customer(body)
-  // Todo: implement cab allocation functionality here
+  let resp = allocator.assignCab(cust)
+  switch (resp.statuscode) {
+    case -1: res.send({
+      status: 'failed',
+      desc: 'No Cabs available'
+    })
+      break
+    case 1: res.send({
+      status: 'success',
+      rideid: resp.rideid
+    })
+  }
+})
+
+router.post('/endride', (req, res) => {
+  const body = req.body
+  let invoice = allocator.endRide(body.rideid, new Location(...body.location))
+  res.send(invoice)
 })
 
 module.exports = router
