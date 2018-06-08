89095f0a17c7c709ce4058d24641a6ad8ca0f954
sumit <kumar1993sumit15@gmail.com>
2018-06-08 13:30:38 +0530

    customer & driver profiles info

---
+++ bookingservice/Profiles.js
@@ -0,0 +1,18 @@
+const Person = require('../bookingservice/Person')
+const {Location} = require('../bookingservice/utils')
+// takes object of type person as constructor argument
+class Driver {
+  constructor (person) {
+    this.personalDetails = person
+  }
+}
+
+class Customer {
+  constructor (requestParams) {
+    this.userid = requestParams.userid
+    this.location = new Location(...requestParams.location)
+    this.colorPref = requestParams.color
+  }
+}
+
+module.exports = {Customer, Driver}
