66746c1ac239c5df38867fb00eed7c36115577b3
sumit <kumar1993sumit15@gmail.com>
2018-06-08 21:40:41 +0530

    fixes pricing, distance & price precision

--- bookingservice/AllocatorService.js
+++ bookingservice/AllocatorService.js
@@ -1,8 +1,11 @@
+const Cab = require('./Cab')
+const {Car, Location} = require('./utils')
+const {Customer, Driver} = require('./Profiles')
 class Ride {
   constructor (pickup, customer, cab) {
     this.pickupLocation = pickup // object of Location class
     this.customer = customer
-    this.cab = cab  // Object of Cab
+    this.cab = cab // Object of Cab
     this.rideId = Math.floor(Math.random() * 100000)
     this.startTime = Date.now()
     this.endTime = null
@@ -17,17 +20,18 @@ class Ride {
   rideDistance () {
     let l1 = this.pickupLocation
     let l2 = this.destination
-    return Math.sqrt(Math.pow(l1.lat - l2.lat, 2) + Math.pow(l1.long - l2.long, 2))
+    let dist = Math.sqrt(Math.pow(l1.lat - l2.lat, 2) + Math.pow(l1.long - l2.long, 2))
+    return dist.toFixed(2)
   }
 
-  totalTime () { return (this.endTime - this.startTime) / 60 }
+  totalTime () { return (this.endTime - this.startTime) / 1000 / 60 }
 
   calcAmount () {
     let _time = this.totalTime()
     let _dist = this.rideDistance()
     let amount = _time + (_dist * 2)
     if (this.cab.vehicle.color === 'pink') amount += 5
-    return amount
+    return amount.toFixed(2)
   }
 }
 
@@ -97,7 +101,7 @@ class AllocatorService {
     ride.endRide(loc)
 
     let cab = ride.cab
-    cab.setLocation(loc)   // ride ends
+    cab.setLocation(loc) // ride ends
     this.addCabToPool(cab) // cab is ready to be added to pool again
     let invoice = this.rideInvoice(ride)
     return invoice
