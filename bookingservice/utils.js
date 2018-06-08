7a6447a3196b3b1e95a01c4727a4d9ca1714a93d
sumit <kumar1993sumit15@gmail.com>
2018-06-08 13:31:18 +0530

    car class model

--- bookingservice/utils.js
+++ bookingservice/utils.js
@@ -5,6 +5,15 @@ class Location {
   }
 }
 
+class Car {
+  constructor (model, regNo, color, capacity) {
+    this.model = model
+    this.regNo = regNo
+    this.color = color
+    this.capacity = capacity
+  }
+}
+
 module.exports = {
-  Location
+  Location, Car
 }
