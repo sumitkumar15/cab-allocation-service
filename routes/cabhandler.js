d9e43d851d579a41c81f2b7c48f3bfccf407dc4b
sumit <kumar1993sumit15@gmail.com>
2018-06-08 17:14:36 +0530

    handler to hire cab

--- routes/cabhandler.js
+++ routes/cabhandler.js
@@ -1,12 +1,15 @@
 var express = require('express')
 var router = express.Router()
 var allocator = require('../bookingservice/AllocatorService')
+const {Customer} = require('../bookingservice/Profiles')
 const bodyParser = require('body-parser')
+
 router.use(bodyParser.json())
 
-router.post('/:userid', (req, res) => {
+router.post('/hire', (req, res) => {
   const params = req.params
   const body = req.body
+  var cust = new Customer(body)
   // Todo: implement cab allocation functionality here
 })
 
